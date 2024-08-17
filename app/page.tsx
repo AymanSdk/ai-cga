7269417918f4c628e71d2dd540de657e4b5f6b31
Aymansdk <ayman.sdk123@outlook.com>
2024-08-17 15:34:04 +0100

    chore: Remove unnecessary heading in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <h2> P-t Test </h2>
+      <h2></h2>
       <Button> Click Here </Button>
     </div>
   );
