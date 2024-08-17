5abeefe852f8b8083aa2337443d140a48d2dad38
Aymansdk <ayman.sdk123@outlook.com>
2024-08-17 16:08:32 +0100

    feat: Add OutputSection component to display user result

--- app/dashboard/content/_components/OutputSection.tsx
+++ app/dashboard/content/_components/OutputSection.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useRef } from "react";
+import "@toast-ui/editor/dist/toastui-editor.css";
+import { Editor } from "@toast-ui/react-editor";
+import { Button } from "@/components/ui/button";
+import { CopyIcon } from "lucide-react";
 
 function OutputSection() {
-  return <div>output section</div>;
+  const editorRef: any = useRef();
+
+  return (
+    <div className="bg-white shadow-lg border rounded-lg">
+      <div className="flex justify-between items-center p-5">
+        <h2 className="font-medium text-lg">Your Result</h2>
+        <Button className="flex gap-2">
+          <CopyIcon className="w-4 h-4" />
+          Copy
+        </Button>
+      </div>
+      <Editor
+        // previewStyle="vertical"
+        ref={editorRef}
+        initialValue="Edit Text here"
+        height="600px"
+        initialEditType="wysiwyg"
+        useCommandShortcut={true}
+        onChange={() =>
+          console.log(editorRef.current.getInstance().getMarkdown())
+        }
+      />
+    </div>
+  );
 }
 
 export default OutputSection;
