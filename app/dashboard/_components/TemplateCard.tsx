962a18d72e5515f6ba11e81e73ced09119b65378
Aymansdk <ayman.sdk123@outlook.com>
2024-08-17 07:07:01 +0100

    chore: Update TemplateCard component to fix link in TemplateCard.tsx

--- app/dashboard/_components/TemplateCard.tsx
+++ app/dashboard/_components/TemplateCard.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 function TemplateCard(item: TEMPLATE) {
   return (
-    <Link href={"/dashboard/content" + item?.slug}>
+    <Link href={"/dashboard/content/" + item?.slug}>
       <div className="p-5 shadow-md border bg-white flex flex-col gap-3 cursor-pointer hover:scale-105 transition-all">
         <Image src={item.icon} alt="icon" width={50} height={50} />
         <h2 className="font-medium text-lg">{item.name}</h2>
