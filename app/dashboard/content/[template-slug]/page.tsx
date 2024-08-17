94aab31f777cfd81f531af71a489ad218d28d56b
Aymansdk <ayman.sdk123@outlook.com>
2024-08-17 15:33:56 +0100

    Update CreateNewContent page layout to include Back button

--- app/dashboard/content/[template-slug]/page.tsx
+++ app/dashboard/content/[template-slug]/page.tsx
@@ -1,8 +1,13 @@
+"use client";
+
 import React from "react";
 import FormSection from "../_components/FormSection";
 import OutputSection from "../_components/OutputSection";
 import Templates from "@/app/(data)/Templates";
 import { TEMPLATE } from "../../_components/TemplateListSection";
+import { Button } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
 
 interface PROPS {
   params: {
@@ -15,13 +20,27 @@ function CreateNewContent(props: PROPS) {
     (item) => item.slug == props.params["template-slug"]
   );
 
+  const GenerateAIcontent = (FormData: any) => {};
+
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-5 p-5">
-      {/* Form Section  */}
-      <FormSection selectedTemplate={seletedTemplate} />
-      {/* Output setion */}
-      <div className="col-span-2">
-        <OutputSection />
+    <div className="p-10">
+      <Link href={"/dashboard"}>
+        <Button>
+          <ArrowLeft />
+          Back
+        </Button>
+      </Link>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-5 py-5">
+        {/* Form Section  */}
+        <FormSection
+          selectedTemplate={seletedTemplate}
+          userFormInput={(v: any) => GenerateAIcontent(v)}
+        />
+
+        {/* Output setion */}
+        <div className="col-span-2">
+          <OutputSection />
+        </div>
       </div>
     </div>
   );
