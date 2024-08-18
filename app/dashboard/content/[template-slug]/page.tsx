9db2205426f84ca92c87d63be7cf4a6ee3918fa0
Aymansdk <ayman.sdk123@outlook.com>
2024-08-18 01:45:59 +0100

    feat: Add moment.js for date formatting and saving AI response to database

--- app/dashboard/content/[template-slug]/page.tsx
+++ app/dashboard/content/[template-slug]/page.tsx
@@ -1,14 +1,18 @@
 "use client";
 
-import React, { useState } from "react";
+import moment from "moment";
+import Link from "next/link";
 import FormSection from "../_components/FormSection";
 import OutputSection from "../_components/OutputSection";
 import Templates from "@/app/(data)/Templates";
-import { TEMPLATE } from "../../_components/TemplateListSection";
+import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
+import { TEMPLATE } from "../../_components/TemplateListSection";
 import { ArrowLeft } from "lucide-react";
-import Link from "next/link";
 import { chatSession } from "@/utils/AiModal";
+import { AIOutput } from "@/utils/schema";
+import { db } from "@/utils/db";
+import { useUser } from "@clerk/nextjs";
 
 interface PROPS {
   params: {
@@ -17,26 +21,36 @@ interface PROPS {
 }
 
 function CreateNewContent(props: PROPS) {
-  const seletedTemplate: TEMPLATE | undefined = Templates?.find(
+  const selectedTemplate: TEMPLATE | undefined = Templates?.find(
     (item) => item.slug == props.params["template-slug"]
   );
-
   const [loading, setLoading] = useState(false);
-
   const [aiOutput, setAiOutput] = useState<string>("");
-
+  const { user } = useUser();
   const GenerateAIcontent = async (formData: any) => {
     setLoading(true);
 
-    const SelectedPrompt = seletedTemplate?.aiPrompt;
+    const SelectedPrompt = selectedTemplate?.aiPrompt;
 
     const FinalAIPrompt = JSON.stringify(formData) + ", " + SelectedPrompt;
 
     const result = await chatSession.sendMessage(FinalAIPrompt);
 
-    console.log(result.response.text());
-    setAiOutput(result.response.text());
+    setAiOutput(result?.response.text());
+    await SaveInDb(formData, selectedTemplate?.slug, result?.response.text());
     setLoading(false);
+  };
+
+  const SaveInDb = async (formData: any, slug: any, aiResp: string) => {
+    const result = await db.insert(AIOutput).values({
+      formData: formData,
+      templateSlug: slug,
+      aiResponse: aiResp,
+      createdBy: user?.primaryEmailAddress?.emailAddress,
+      createdAt: moment().format("DD/MM/YYYY"),
+    });
+
+    console.log(result);
   };
 
   return (
@@ -50,7 +64,7 @@ function CreateNewContent(props: PROPS) {
       <div className="grid grid-cols-1 md:grid-cols-3 gap-5 py-5">
         {/* Form Section  */}
         <FormSection
-          selectedTemplate={seletedTemplate}
+          selectedTemplate={selectedTemplate}
           userFormInput={(v: any) => GenerateAIcontent(v)}
           loading={loading}
         />
